ec82c58d43a918164a839fdbac331284d6083d45
Amy McCarthy <amy.mccarthy@cainthus.com>
2022-03-28 15:25:27 +0100

    Updates

---
+++ static/js/620.18199980.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpwa_test=self.webpackChunkpwa_test||[]).push([[620],{620:function(e,n,t){t.r(n);t(791),t(508);var s=t(184);n.default=function(){return(0,s.jsx)("div",{className:"App",children:"page one"})}},508:function(){}}]);
+//# sourceMappingURL=620.18199980.chunk.js.map
